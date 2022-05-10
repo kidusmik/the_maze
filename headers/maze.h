cb256a426e61ca260d26505876cbcddb9500825b
kidusmik <kidusmik@gmail.com>
2022-05-10 07:37:08 +0000

    Add maze.h with an SDL instance structure

---
+++ headers/maze.h
@@ -0,0 +1,25 @@
+#ifndef _MAZE_H_
+#define _MAZE_H_
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct SDL_Instance - SDL instance
+ * @window: the window to render to
+ * @renderer: the renderer
+ *
+ * Description: SDL instance structure
+ *
+ */
+typedef struct SDL_Instance
+{
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+} SDL_Instance;
+
+int init_instance(SDL_Instance *);
+void draw_figure(SDL_Instance instance);
+
+#endif /* _MAZE_H_ */
